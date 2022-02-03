28e94935a4f2219579efea2d25114fb7c75e0961
riham1002 <re.khalaily@gmail.com>
2022-02-03 11:55:49 +0200

    filters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Filter from './components/Filter'
-import DishesList from './components/DishesList';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PriceFilter from "./components/PriceFilter";
+import DishesList from "./components/DishesList";
+import CatFilter from "./components/CatFilter";
 
 function App() {
+  const [priceFilter, setPriceFilter] = React.useState([0.5, 9]);
+  const [catFilter, setCatFilter] = React.useState("all");
+
   return (
     <div className="App">
-     <h1> Hand to mouth resturant </h1>
-     <Filter> </Filter>
-     <DishesList> </DishesList>
+      <h1> Hand to mouth resturant </h1>
+      <PriceFilter
+        priceFilter={priceFilter}
+        setPriceFilter={setPriceFilter}
+      ></PriceFilter>
+      <CatFilter catFilter={catFilter} setCatFilter={setCatFilter}></CatFilter>
+      <DishesList catFilter={catFilter} priceFilter={priceFilter}>
+        {" "}
+      </DishesList>
     </div>
   );
 }
--- src/components/DishesList.jsx
+++ src/components/DishesList.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React from "react";
 import dishes from "../database/data";
-import Dish from './Dish';
-import '../style/DishList.css'
-function DishesList (){
-    return (
-        <ul>
-      {dishes.map((dish) => (
-       <Dish key={dish.id} name={dish.name} category={dish.category} price={dish.price} description={dish.description} img={dish.image} > </Dish>
-       ))}
-       </ul>
-    )
+import Dish from "./Dish";
+import "../style/DishList.css";
+function DishesList({ priceFilter, catFilter }) {
+  const dishItems = dishes;
+  const [minPrice, maxPrice] = priceFilter;
+
+  return (
+    <ul>
+      {dishItems
+        .filter((dish) => dish.category === catFilter || catFilter === "all")
+        .filter((dish) => dish.price >= minPrice && dish.price <= maxPrice)
+        .map((dish) => (
+          <Dish
+            key={dish.id}
+            name={dish.name}
+            category={dish.category}
+            price={dish.price}
+            description={dish.description}
+            img={dish.image}
+          ></Dish>
+        ))}
+    </ul>
+  );
 }
 
 export default DishesList;
